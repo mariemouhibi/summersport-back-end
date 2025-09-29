792b5d37be5cb4c27144b180f97405ce3956f9fd
Whibi Mariem <126107813+mariemouhibi@users.noreply.github.com>
2025-09-29 19:28:53 +0200

    Add files via upload

---
+++ SummersportApplication.java
@@ -0,0 +1,13 @@
+package com.example.crocojuinspring.summersport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummersportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummersportApplication.class, args);
+	}
+
+}
